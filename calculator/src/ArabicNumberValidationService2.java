8c668e22177c0d74e40876e288562fb1c10edeaa
BWbear1 <blackwhitebear1@gmail.com>
2023-07-31 06:05:45 +0200

    душот3

--- calculator/src/ArabicNumberValidationService2.java
+++ calculator/src/ArabicNumberValidationService2.java
@@ -1,6 +1,4 @@
 public class ArabicNumberValidationService2 {
-    String ERROR6 = "т.к введены некорректные данные, допустимый диапазон значений [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10][I, II, III, IV, V, VI, VII, VIII, IX, X]";
-
     boolean arabicNumberIsCorrect2(String line1) {
         int number1;
         try {
